8fa9c571697dfebd598f44656c6e5dc046dfa569
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2024-08-05 20:10:06 -0300

    FIX - Get Products of Users Without the SELLER Roule

--- src/services/product/getProductsBySellerIdUsecase.js
+++ src/services/product/getProductsBySellerIdUsecase.js
@@ -1,7 +1,13 @@
 import { getProductsBySellerId } from "../../repositories/productRepository.js";
+import { getUserById } from "../../repositories/userRepository.js";
+import CustomError from "../../utils/errors/customError.js";
 
 async function getProductsBySellerIdUsecase(sellerId) {
-    // FIXME: If seller is not found, returning 404
+    const user = await getUserById(sellerId);
+    if(!user) throw new CustomError(404, "Seller not found");
+
+    if(user.role !== "SELLER") throw new CustomError(400, "This user is not a seller");
+
     return await getProductsBySellerId(sellerId);
 }
 
