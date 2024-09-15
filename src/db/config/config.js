03342d24fc1c69ae07fe87216ee67146592bd5ce
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2024-09-15 10:29:40 -0300

    ADD - Production Variables in DB Config

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -10,4 +10,12 @@ module.exports = {
         "port": process.env.POSTGRESQL_PORT,
         "dialect": "postgres",
     },
+    "production": {
+        "database": process.env.POSTGRESQL_DATABASE,
+        "username": process.env.POSTGRESQL_USER,
+        "password": process.env.POSTGRESQL_PASSWORD,
+        "host": process.env.POSTGRESQL_HOST,
+        "port": process.env.POSTGRESQL_PORT,
+        "dialect": "postgres",
+    }
 };
