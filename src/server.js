464c80ed700a901699c0425a6380fc6473c0b3bf
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2024-12-07 22:00:14 -0300

    REM - Inactivity in Render

--- src/server.js
+++ src/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import cron from "node-cron";
-import http from "http";
 
 import routes from "./routes/routes.js";
 import "./redis/redisClient.js";
@@ -21,26 +19,4 @@ app.use(routes);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-});
-
-// Service for avoid inactivity in Backend Deploy
-cron.schedule("* * * * *", () => {
-    const options = {
-        hostname: "localhost",
-        port: 3000,
-        path: "/check",
-        method: "GET",
-    };
-
-    const req = http.request(options, (res) => {
-        res.on("end", () => {
-            console.log("Success on check");
-        });
-    });
-
-    req.on("error", (error) => {
-        console.error("Error on check:", error);
-    });
-
-    req.end();
 });
